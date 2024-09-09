fb04a2a63c31b45b69cf0e08c58c413b6fa1e3e1
Boris <kabv@mail.ru>
2024-09-09 19:20:08 +0600

    commit2

--- client.py
+++ client.py
@@ -21,12 +21,61 @@ async def get_people(people_id, session):
     return json_data
 
 
-async def insert_people(people_list):
+async def get_film(films, session):
+    list_films = []
+    for film in films:
+        response = await session.get(
+            f'{film}',
+        )
+        json_data = await response.json()
+        list_films.append(json_data['title'])
+    return {'films': ','.join(list_films)}
+
+
+async def get_species(species, session):
+    list_species = []
+    for specie in species:
+        response = await session.get(
+            f'{specie}',
+        )
+        json_data = await response.json()
+        list_species.append(json_data['name'])
+    return {'species': ','.join(list_species)}
+
+
+async def get_starships(starships, session):
+    list_starships = []
+    for starship in starships:
+        response = await session.get(
+            f'{starship}',
+        )
+        json_data = await response.json()
+        list_starships.append(json_data['name'])
+    return {'starships': ','.join(list_starships)}
+
+
+async def get_vehicles(vehicles, session):
+    list_vehicles = []
+    for vehicle in vehicles:
+        response = await session.get(
+            f'{vehicle}',
+        )
+        json_data = await response.json()
+        list_vehicles.append(json_data['name'])
+    return {'starships': ','.join(list_vehicles)}
+
+
+async def insert_people(people_list, session_http):
     async with SessionDB() as session:
-        orm_model_list = [
-            Сharacter(json=people_dict)
-            for people_dict in people_list
-        ]
+        orm_model_list = []
+        for people_dict in people_list:
+            people_dict['films'] = await get_film(people_dict['films'], session_http)
+            people_dict['species'] = await get_species(people_dict['species'], session_http)
+            people_dict['starships'] = await get_starships(people_dict['starships'], session_http)
+            people_dict['vehicles'] = await get_vehicles(people_dict['vehicles'], session_http)
+
+            orm_model_list.append(Сharacter(json=people_dict))
+
         session.add_all(orm_model_list)
         await session.commit()
 
@@ -37,7 +86,7 @@ async def main():
         coros = (get_people(i, session) for i in range(1, 100_001))
         for coros_chunk in more_itertools.chunked(coros, 5):
             people_list = await asyncio.gather(*coros_chunk)
-            await insert_people(people_list)
+            await insert_people(people_list, session)
             print(people_list)
 
 
